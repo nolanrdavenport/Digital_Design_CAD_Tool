d3fca706778dac1cb3672d2484234a25eac287c2
nolanrdavenport <54985953+nolanrdavenport@users.noreply.github.com>
2020-06-03 12:59:25 -0500

    Added some images and started the schematic system

--- Digital Design CAD Tool/src/Main.java
+++ Digital Design CAD Tool/src/Main.java
@@ -5,14 +5,21 @@ import javafx.geometry.Orientation;
 import javafx.stage.Stage;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
@@ -28,18 +35,28 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-		
+		// General Setup
 		primaryStage.setTitle("Digital Design CAD Tool");
 		MenuBar menuBar = setupMenuBar();
 		SplitPane splitPane = new SplitPane();
 		TabPane tabPane = new TabPane();
-		ToolBar toolBar = setupToolBar();
-		
 		HBox tabContainer = new HBox(tabPane);
-		HBox toolContainer = new HBox(toolBar);
+		ScrollPane toolScrollPane = new ScrollPane();
+		VBox toolContainer_vert1 = new VBox();
+		setupToolBarLeft(toolContainer_vert1);
+		VBox toolContainer_vert2 = new VBox();
+		setupToolBarRight(toolContainer_vert2);
+		HBox toolContainer = new HBox();
+		toolContainer.getChildren().add(toolContainer_vert1);
+		toolContainer.getChildren().add(new Separator());
+		toolContainer.getChildren().add(toolContainer_vert2);
+		toolScrollPane.setContent(toolContainer);
+		splitPane.getItems().addAll(toolScrollPane, tabContainer);
+		this.primaryStage = primaryStage;
+		this.menuBar = menuBar;
+		this.tabPane = tabPane;
 		
-		splitPane.getItems().addAll(toolContainer, tabContainer);
+		//For setting the divider position at the start of the program
 		primaryStage.showingProperty().addListener(new ChangeListener<Boolean>() {
 
 		    @Override
@@ -52,9 +69,6 @@ public class Main extends Application{
 		    }
 
 		});
-		this.primaryStage = primaryStage;
-		this.menuBar = menuBar;
-		this.tabPane = tabPane;
 		
 		//adding welcome text tab
 		TextArea welcomeText = new TextArea();
@@ -64,25 +78,28 @@ public class Main extends Application{
 		Tab welcomeTab = new Tab("Welcome", welcomeText);
 		tabPane.getTabs().add(welcomeTab);
 		
-		
+		// Add menu bar and split pane to the scene
 		VBox vBox = new VBox(menuBar);
 		vBox.getChildren().add(splitPane);
 		Scene scene = new Scene(vBox, 500, 500);
 		
+		// Adjust divider position whenever the frame is changed in width
 		scene.widthProperty().addListener(new ChangeListener<Number>() {
 		    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
 		    	splitPane.setDividerPositions(0.1);
 		    }
 		});
+		
+		// Adjust the split pane height whenever the frame is changed in height
 		scene.heightProperty().addListener(new ChangeListener<Number>() {
 		    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
 		    	splitPane.setPrefHeight(primaryStage.getHeight());
 		    }
 		});
-		primaryStage.setMaximized(true);
 		
+		// Finalize stage initialization and show stage
+		primaryStage.setMaximized(true);
 		scene.getStylesheets().add("Styles/primary_style.css");
-
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
@@ -138,23 +155,49 @@ public class Main extends Application{
 		return menuBar;
 	}
 	
-	public ToolBar setupToolBar() {
-		ToolBar toolBar = new ToolBar();
-		Button temp1 = new Button("[temp1]");
-		Button temp2 = new Button("[temp2]");
-		toolBar.setOrientation(Orientation.VERTICAL);
+	/*
+	 * These are methods that set up the tool bar.
+	 * TODO: MAKE A BETTER VERSION OF THIS SHIT. This is temp
+	 */
+	public void setupToolBarLeft(VBox container) {
+		Image andImage = new Image("Images/AND_gate.png", 60, 60, false, false);
+		Button andButton = new Button();
+		andButton.setGraphic(new ImageView(andImage));
+		container.getChildren().add(andButton);
 		
-		toolBar.getItems().add(temp1);
-		toolBar.getItems().add(temp2);
+		Image orImage = new Image("Images/OR_gate.png", 60, 60, false, false);
+		Button orButton = new Button();
+		orButton.setGraphic(new ImageView(orImage));
+		container.getChildren().add(orButton);
+	}
+	public void setupToolBarRight(VBox container) {
+		Image nandImage = new Image("Images/NAND_gate.png", 60, 60, false, false);
+		Button nandButton = new Button();
+		nandButton.setGraphic(new ImageView(nandImage));
+		container.getChildren().add(nandButton);
 		
-		return toolBar;
+		Image norImage = new Image("Images/NOR_gate.png", 60, 60, false, false);
+		Button norButton = new Button();
+		norButton.setGraphic(new ImageView(norImage));
+		container.getChildren().add(norButton);
 	}
 	
 	/*
 	 * This method creates a new schematic and adds it to the tab pane. 
 	 */
 	public void createSchematic() {
+		/*
 		Tab newSchematic = new Tab("schematic", new Label("[TODO: make schematic system]"));
+		tabPane.getTabs().add(newSchematic);
+		*/
+		ScrollPane sPane = new ScrollPane();
+		Canvas schematicCanvas = new Canvas(2000,2000);
+		GraphicsContext gc = schematicCanvas.getGraphicsContext2D();
+		gc.fillRect(100,  100, 500, 500);
+		gc.setFill(Color.GREEN);
+		gc.fillRect(200, 200, 50, 50);
+		sPane.setContent(schematicCanvas);
+		Tab newSchematic = new Tab("schematic", sPane);
 		tabPane.getTabs().add(newSchematic);
 	}
 }
